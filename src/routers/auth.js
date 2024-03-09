b1ab5ac7ed32352e9b909315e63890253a99dabd
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-09 16:47:37 +0700

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -19,27 +19,24 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Configure Passport
-configurePassport();
+//configurePassport();
 
 // Routes
-app.get('/login', authenticateWithGoogle);
+/*app.get('/login', authenticateWithGoogle);
 
 app.get('/auth/google/callback', handleGoogleCallback, (req, res) => {
   const redirectTo = req.session.returnTo || '/auth/profile';
   delete req.session.returnTo; // Hapus setelah digunakan
   res.redirect(redirectTo);
 });
+*/
 
-app.get('/auth/profile', ensureAuthenticated, (req, res) => {
+app.get('/auth/profile', (req, res) => {
   res.render('./pages/auth/profile');
 });
 
 app.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect('/auth/profile');
-  } else {
     res.render('./pages/main/landing');
-  }
 });
 
 app.get('/logout', (req, res) => {
