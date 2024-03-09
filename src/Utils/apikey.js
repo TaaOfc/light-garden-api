cb974a53bab319f15bf88275544aaf6dc6c07209
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-09 17:14:44 +0700

    Update apikey.js

--- src/Utils/apikey.js
+++ src/Utils/apikey.js
@@ -1,3 +1,16 @@
+const apikeyList = ['okta', 'lann'];//input key disini
+
+const cekApikey = async (req, res, next) => {
+    const apikey = req.query.apikey;
+
+    if (!apikey) return res.json({ creator, status: false, message: 'apikey parameter cannot be empty.' });
+
+    if (!apikeyList.includes(apikey)) return res.json({ creator, status: false, message: `apikey ${apikey} not found.` });
+
+    next();
+}
+
+
 const isApikeyValid = (apikey) => {
     for (const email in db.data.users) {
         if (db.data.users[email].apikey === apikey) {
@@ -6,17 +19,6 @@ const isApikeyValid = (apikey) => {
     }
     return false;
 };
-
-const cekApikey = async (req, res, next) => {
-    const apikey = req.query.apikey;
-    const b = await isApikeyValid(apikey)
-
-    if (!apikey) return res.json({ creator, status: false, message: 'apikey parameter cannot be empty.' });
-    if (!b) return res.json({ creator, status: false, message: `apikey ${apikey} not found.` });
-
-
-    next();
-}
 
 const limitApikey = async (req, res, next) => {
     const apikey = req.query.apikey;
