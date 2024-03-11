31f1ad477c58e08ff5bf35d87cdbb1c50130c598
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-11 22:22:27 +0700

    Update game.js

--- src/routers/api/game.js
+++ src/routers/api/game.js
@@ -5,30 +5,13 @@ import { tebakkata } from "../../Utils/scapers/all-in-one/scapers.js";
 const router = express.Router();
 
 router.get('/api/game/tebakkata', cekApikey, async(req, res) => {
-    try {
-        const q = req.query.q;
-
-        if (!q) return res.json(global.mess.need.q);
-
-        const data = await tebakkata(q);
-
-        if (!data) {
-            return res.json({
-                creator,
-                status: false,
-                message: "NOT FOUND"
-            });
-        }
-
-        res.json({
-            creator,
-            status: true,
-            result: data
-        });
-    } catch (error) {
-        console.error(error);
-        res.json(mess.error);
-    }
+    let ra = await fetchJson('https://raw.githubusercontent.com/AlipBot/data-rest-api/main/tebakgame.json')
+	let ha = ra[Math.floor(Math.random() * ra.length)]
+  res.json({
+	status: true,
+	creator: `${creator}`,
+	result: ha
+})
 });
 
 export default router;
