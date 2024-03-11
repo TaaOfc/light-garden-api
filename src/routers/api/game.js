e5729dff471eeb06163d1066188d24da418a9ddb
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-11 17:13:25 +0700

    Update game.js

--- src/routers/api/game.js
+++ src/routers/api/game.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { cekApikey, limitApikey } from "../../Utils/apikey.js"
-import { hentaivid } from "../../Utils/scapers/all-in-one/scapers.js";
+import { tebakkata } from "../../Utils/scapers/all-in-one/scapers.js";
 
 const router = express.Router();
 
-router.get('/api/internet/hentaivid', cekApikey, async(req, res) => {
+router.get('/api/game/tebakkata', cekApikey, async(req, res) => {
     try {
         const q = req.query.q;
 
         if (!q) return res.json(global.mess.need.q);
 
-        const data = await pinterest(q);
+        const data = await tebakkata(q);
 
         if (!data) {
             return res.json({
