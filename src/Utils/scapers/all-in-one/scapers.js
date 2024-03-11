d896b3f80a3e2dbb856426f5dd2f1a721cf5bedb
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-12 05:54:21 +0700

    Update scapers.js

--- src/Utils/scapers/all-in-one/scapers.js
+++ src/Utils/scapers/all-in-one/scapers.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import cheerio from "cheerio";
 import ytdl from 'ytdl-core';
 import fs from 'fs'
-import fetchJson from '../../../lib/myfunc.js'
+import fetchJson from '../../../lib/func.js'
 import fetch from 'node-fetch'
 
 
