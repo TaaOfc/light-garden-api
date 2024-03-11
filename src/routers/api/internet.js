64862f9069fe8fe333797f3dc67984a736dfb9ae
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-11 13:56:48 +0700

    Update internet.js

--- src/routers/api/internet.js
+++ src/routers/api/internet.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { cekApikey, limitApikey } from "../../Utils/apikey.js"
-import { hentai } from "../../Utils/scapers/all-in-one/scapers.js";
+import { hentai, nhentai } from "../../Utils/scapers/all-in-one/scapers.js";
 
 const router = express.Router();
 
@@ -31,5 +31,31 @@ router.get('/api/internet/hentai', cekApikey, async(req, res) => {
     }
 });
 
+router.get('/api/internet/nhentai', cekApikey, async(req, res) => {
+    try {
+        const q = req.query.q;
+
+        if (!q) return res.json(global.mess.need.q);
+
+        const data = await nhentai(q);
+
+        if (!data) {
+            return res.json({
+                creator,
+                status: false,
+                message: "NOT FOUND"
+            });
+        }
+
+        res.json({
+            creator,
+            status: true,
+            result: data
+        });
+    } catch (error) {
+        console.error(error);
+        res.json(mess.error);
+    }
+});
 
 export default router; 
