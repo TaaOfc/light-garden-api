545599f75bb9b035ab6c98a3c00e98a53efba6d3
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-12 10:34:55 +0700

    Update internet.js

--- src/routers/api/internet.js
+++ src/routers/api/internet.js
@@ -1,8 +1,35 @@
 import express from "express";
 import { cekApikey, limitApikey } from "../../Utils/apikey.js"
-import { hentai, nhentai, milf} from "../../Utils/scapers/all-in-one/scapers.js";
+import { bardAi, hentai, nhentai, milf} from "../../Utils/scapers/all-in-one/scapers.js";
 
 const router = express.Router();
+
+router.get('/api/internet/bardAi', cekApikey, async(req, res) => {
+    try {
+        const q = req.query.q;
+
+        if (!q) return res.json(global.mess.need.q);
+
+        const data = await bardAi(q);
+
+        if (!data) {
+            return res.json({
+                creator,
+                status: false,
+                message: "NOT FOUND"
+            });
+        }
+
+        res.json({
+            creator,
+            status: true,
+            result: data
+        });
+    } catch (error) {
+        console.error(error);
+        res.json(mess.error);
+    }
+});
 
 router.get('/api/internet/hentai', cekApikey, async(req, res) => {
     try {
