fc2d1a518159e2c3b203e7d77fd2a811b8f5d491
TaaOfc <162397498+TaaOfc@users.noreply.github.com>
2024-03-12 10:48:58 +0700

    Update internet.js

--- src/routers/api/internet.js
+++ src/routers/api/internet.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { cekApikey, limitApikey } from "../../Utils/apikey.js"
 import { hentai, nhentai, milf} from "../../Utils/scapers/all-in-one/scapers.js";
-import { bardApi } from "../../bardApi.js"
+import { bardApi } from "../../Utils/scapers/bardApi.js"
 const router = express.Router();
 
 router.get('/api/internet/bardAi', cekApikey, async(req, res) => {
